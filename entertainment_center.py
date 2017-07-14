018b9780b9108b3686b9b12ecb44f9d3d22c14cf
TanyaGir <tanyagir24@gmail.com>
2017-07-14 15:49:29 -0500

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,58 @@
+import media
+import fresh_tomatoes
+
+
+# create list of movies to display in the HTML file
+finding_dory = media.Movie(
+    "Finding Dory",
+    "The story about a fish with who sets out to find her parents",
+    "https://upload.wikimedia.org/wikipedia/en/3/3e/Finding_Dory.jpg",
+    "https://www.youtube.com/watch?v=MKJA- VLpiCo")
+
+
+beauty_and_the_beast = media.Movie(
+    "Beauty and the Beast",
+    "Story of a girl who falls in love with a beast",
+    ("https://upload.wikimedia.org/wikipedia/en/d/d6/"
+     "Beauty_and_the_Beast_2017_ poster.jpg"),
+    "https://www.youtube.com/watch?v=e3Nl_TCQXuw")
+
+finding_nemo = media.Movie(
+    "Finding Nemo",
+    "The story about how a lost fish finds his way back home",
+    "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
+    "https://www.youtube.com/watch?v=wZdpNglLbt8")
+
+
+zootopia = media.Movie(
+    "Zootopia",
+    "A story abou a bunny who sets out change what everyone thinks of her",
+    "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
+    "https://www.youtube.com/watch?v=jWM0ct-OLsM")
+
+
+ratatouille = media.Movie(
+    "Ratatouille",
+    "The story about a rat",
+    "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+kung_fu_panda = media.Movie(
+    "Kung Fu Panda",
+    "The story about a Panda",
+    "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
+    "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
+
+# create a list of the instances created above
+movies = [
+    finding_dory,
+    beauty_and_the_beast,
+    finding_nemo,
+    zootopia,
+    ratatouille,
+    kung_fu_panda]
+
+
+'''open_movies_page is a function in fresh_tomatoes.py that takes in one
+argumentwhich is the list of movies'''
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,25 @@
+'''imports the webrowser file to make it
+available to use within the current file'''
+import webbrowser
+
+
+# data structure called Movie is created
+class Movie():
+
+        ''' creates the __init__ function which initializes the variables
+            of the data structure'''
+        def __init__(
+            self,
+            movie_title,
+            movie_storyline,
+            poster_image,
+            trailer_youtube):
+                self.title = movie_title
+                self.storyline = movie_storyline
+                self.poster_image_url = poster_image
+                self.trailer_youtube_url = trailer_youtube
+
+        ''' creates a function called show_trailer that opens the trailer
+            of the movie in a new browser'''
+        def show_trailer(self):
+            webbrowser.open(self.trailer_youtube_url)
